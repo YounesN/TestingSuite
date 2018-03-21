1e7785b8411a160d222780cb869e38c83e4d7a17
YounesN <younes@wayne.edu>
2018-03-20 21:17:22 -0400

    finished copying scripts to folders

--- Github/githubmaster.sh
+++ Github/githubmaster.sh
@@ -34,89 +34,149 @@ echo -e "====  Copying executables and job scripts to serial"
 directories=()
 find ./serial -name "in.conf" -printf '%h\n' | sort -u > tmpfile
 while IFS= read -r -d $'\n'; do
-    directories+=("$REPLY")
+  directories+=("$REPLY")
 done < tmpfile
 rm -rf tmpfile
 
 for i in "${directories[@]}"
 do
-    if [[ "$i" =~ "GCMC" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_GCMC "$i"
-	cp ./job_scripts/serial.inp "$i"
-	root=`pwd`
-	cd $i
-	sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
-	sed -i "s#COMMAND#./GOMC_CPU_GCMC in.conf \&> out.log#g" serial.inp
-	cd $root
-    elif [[ "$i" =~ "GEMC" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_GEMC "$i"
-	cp ./job_scripts/serial.inp "$i"
-	root=`pwd`
-	cd $i
-	sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
-	sed -i "s#COMMAND#./GOMC_CPU_GEMC in.conf \&> out.log#g" serial.inp
-	cd $root
-    elif [[ "$i" =~ "NVT" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_NVT "$i"
-    elif [[ "$i" =~ "NPT" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_NPT "$i"
-    else
-        echo -e "[ERROR] Couldn't detect the ensemble type. Please include one in the the name of directory"
-        echo -e "\tExample: /input/GCMC/case01/"
-        exit
-    fi
+  if [[ "$i" =~ "GCMC" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_GCMC "$i"
+	  cp ./job_scripts/serial.inp "$i"
+	  root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_GCMC in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "GEMC" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_GEMC "$i"
+	  cp ./job_scripts/serial.inp "$i"
+	  root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_GEMC in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "NVT" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_NVT "$i"
+    cp ./job_scripts/serial.inp "$i"
+	  root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_NVT in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "NPT" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_NPT "$i"
+    cp ./job_scripts/serial.inp "$i"
+	  root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_NPT in.conf \&> out.log#g" serial.inp
+	  cd $root
+  else
+    echo -e "[ERROR] Couldn't detect the ensemble type."
+    echo -e "\tExample: /input/GCMC/case01/"
+    exit
+  fi
 done
 
 ###############################################################################
-echo -e "====  Copying executables to openmp"
+echo -e "====  Copying executables and job scripts to openmp"
 directories=()
 find ./openmp -name "in.conf" -printf '%h\n' | sort -u > tmpfile
 while IFS= read -r -d $'\n'; do
-    directories+=("$REPLY")
+  directories+=("$REPLY")
 done < tmpfile
 rm -rf tmpfile
 
 for i in "${directories[@]}"
 do
-    if [[ "$i" =~ "GCMC" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_GCMC "$i"
-    elif [[ "$i" =~ "GEMC" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_GEMC "$i"
-    elif [[ "$i" =~ "NVT" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_NVT "$i"
-    elif [[ "$i" =~ "NPT" ]]; then
-        cp ./GOMC/bin/GOMC_CPU_NPT "$i"
-    else
-        echo -e "[ERROR] Couldn't detect the ensemble type. Please include in the the name of directory"
-        echo -e "\tExample: /input/GCMC/case01/"
-        exit
-    fi
+  if [[ "$i" =~ "GCMC" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_GCMC "$i"
+    cp ./job_scripts/openmp.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_GCMC +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "GEMC" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_GEMC "$i"
+    cp ./job_scripts/openmp.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_GEMC +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "NVT" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_NVT "$i"
+    cp ./job_scripts/openmp.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_NVT +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "NPT" ]]; then
+    cp ./GOMC/bin/GOMC_CPU_NPT "$i"
+    cp ./job_scripts/openmp.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_NPT +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  else
+    echo -e "[ERROR] Couldn't detect the ensemble type."
+    echo -e "\tExample: /input/GCMC/case01/"
+    exit
+  fi
 done
 
 ###############################################################################
-echo -e "====  Copying executables to gpu"
+echo -e "====  Copying executables and job scripts to gpu"
 directories=()
 find ./gpu -name "in.conf" -printf '%h\n' | sort -u > tmpfile
 while IFS= read -r -d $'\n'; do
-    directories+=("$REPLY")
+  directories+=("$REPLY")
 done < tmpfile
 rm -rf tmpfile
 
 for i in "${directories[@]}"
 do
-    if [[ "$i" =~ "GCMC" ]]; then
-        cp ./GOMC/bin/GOMC_GPU_GCMC "$i"
-    elif [[ "$i" =~ "GEMC" ]]; then
-        cp ./GOMC/bin/GOMC_GPU_GEMC "$i"
-    elif [[ "$i" =~ "NVT" ]]; then
-        cp ./GOMC/bin/GOMC_GPU_NVT "$i"
-    elif [[ "$i" =~ "NPT" ]]; then
-        cp ./GOMC/bin/GOMC_GPU_NPT "$i"
-    else
-        echo -e "[ERROR] Couldn't detect the ensemble type. Please include one in the the name of directory"
-        echo -e "\tExample: /input/GCMC/case01/"
-        exit
-    fi
+  if [[ "$i" =~ "GCMC" ]]; then
+    cp ./GOMC/bin/GOMC_GPU_GCMC "$i"
+    cp ./job_scripts/gpu.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_GCMC +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "GEMC" ]]; then
+    cp ./GOMC/bin/GOMC_GPU_GEMC "$i"
+    cp ./job_scripts/gpu.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_GEMC +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "NVT" ]]; then
+    cp ./GOMC/bin/GOMC_GPU_NVT "$i"
+    cp ./job_scripts/gpu.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_NVT +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  elif [[ "$i" =~ "NPT" ]]; then
+    cp ./GOMC/bin/GOMC_GPU_NPT "$i"
+    cp ./job_scripts/gpu.inp "$i"
+    root=`pwd`
+	  cd $i
+	  sed -i "s#RUN_DIR#$(pwd)#g" serial.inp
+	  sed -i "s#COMMAND#./GOMC_CPU_NPT +p4 in.conf \&> out.log#g" serial.inp
+	  cd $root
+  else
+    echo -e "[ERROR] Couldn't detect the ensemble type."
+    echo -e "\tExample: /input/GCMC/case01/"
+    exit
+  fi
 done
 
 ###############################################################################

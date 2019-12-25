ec60dc6841ba191c1d89af6a9df8f4d5bef3d391
YounesN <younes.nej2008@gmail.com>
2019-12-25 01:56:47 -0500

    separated compile from run

--- Github/runthissecond.sh
+++ Github/runthissecond.sh
@@ -1,47 +1,4 @@
 ###############################################################################
-echo -e "====  Cloning Github Repository"
-git clone https://github.com/GOMC-WSU/GOMC
-cd GOMC
-echo -e "====  Making sure we are on master branch"
-git checkout development
-echo -e "====  Load CUDA and Intel modules"
-module load cuda-8.0/compiler-gpu
-module load intel-16/compiler
-echo -e "====  Compiling the code"
-./metamake.sh
-cd ..
-
-###############################################################################
-echo -e "====  cloning Github Repository for examples"
-git clone https://github.com/GOMC-WSU/GOMC_Examples
-cd GOMC_Examples
-echo -e "====  Making sure we are on master branch"
-git checkout master
-cd ..
-echo -e "====  Replace Random seed with INTSEED"
-directories=()
-find ./GOMC_Examples -name "in.conf" -printf '%h\n' | sort -u > tmpfile
-while IFS= read -r -d $'\n'; do
-    directories+=("$REPLY")
-done < tmpfile
-rm -rf tmpfile
-
-for i in "${directories[@]}"
-do
-    sed -i 's/RANDOM/INTSEED/g' $i/in.conf
-    printf "\n\nRandom_Seed\t50\n\n" >> $i/in.conf
-done
-###############################################################################
-echo -e "====  Removing .git folder first"
-rm -rf ./GOMC_Examples/.git
-echo -e "====  Copying examples to serial"
-rsync -r GOMC_Examples serial
-echo -e "====  Copying examples to openmp"
-rsync -r GOMC_Examples openmp
-echo -e "====  Copying examples to gpu"
-rsync -r GOMC_Examples gpu
-
-###############################################################################
 echo -e "====  Copying executables and job scripts to serial"
 directories=()
 find ./serial -name "in.conf" -printf '%h\n' | sort -u > tmpfile
